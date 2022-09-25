5978a9a96d61a49c0136358a2af629115749b6b7
Henry Thasler <git@thasler.org>
2022-09-25 20:42:24 +0200

    introduce ISR debouncing

--- sensors/metrix NI-3/src/main.cpp
+++ sensors/metrix NI-3/src/main.cpp
@@ -1,11 +1,29 @@
 #include <Arduino.h>
 
+#define REED_INPUT (2)
+
 uint32_t initStage = 0;
-uint32_t timestamp = 0;
+uint64_t timestamp = 0;
+
+bool triggered = false;
+static int32_t debounceTimeout = 300;
+int32_t debounceTimer = 0;
+int32_t triggerCounter = 0;
+
+uint64_t lastIsr = 0;
+uint64_t task100msTimer = 0;
 
 void onInterrupt()
 {
-  Serial.println("[  ISR   ] Trigger!");
+  uint64_t timestamp = millis();
+
+  if ((timestamp - lastIsr) > 150)
+  {
+    digitalWrite(LED_BUILTIN, 1);
+    triggered = true;
+    debounceTimer = timestamp;
+    lastIsr = timestamp;
+  }
 }
 
 void setup()
@@ -22,18 +40,31 @@ void setup()
   Serial.println("[  INIT  ] Begin");
   initStage++;
 
-  attachInterrupt(digitalPinToInterrupt(2), onInterrupt, RISING); //  function for creating external interrupts at pin2 on Rising (LOW to HIGH)
+  attachInterrupt(digitalPinToInterrupt(REED_INPUT), onInterrupt, FALLING); //  function for creating external interrupts at pin2 on Rising (LOW to HIGH)
 }
 
 void loop()
 {
   timestamp = millis();
-  // put your main code here, to run repeatedly:
-  Serial.println(timestamp);
 
-  digitalWrite(LED_BUILTIN, 1); // turn ON LED
-  delay(500);                   // wait for 1 second
-  digitalWrite(LED_BUILTIN, 0); // turn OFF LED
-  delay(500);                   // wait for 1 second
+  if (triggered && ((timestamp - debounceTimer) > debounceTimeout))
+  {
+    if (digitalRead(REED_INPUT))
+    {
+      triggerCounter++;
+      // Serial.print("[  ISR   ] Trigger! ");
+      Serial.print("{\"counter\":");Serial.print(triggerCounter);Serial.print("}");
+    }
+
+    triggered = false;
+    return;
+  }
+
+  // 100ms Task
+  if ((timestamp - task100msTimer) > 100L)
+  {
+    task100msTimer = timestamp;
+    digitalWrite(LED_BUILTIN, 0);
+    return;
+  }
 }
-
