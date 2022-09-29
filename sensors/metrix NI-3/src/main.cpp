9e5dc1aa692b950c9ae89061cb745e0f82e5188a
Henry Thasler <git@thasler.com>
2022-09-29 19:48:05 +0200

    simplify input processing

--- sensors/metrix NI-3/src/main.cpp
+++ sensors/metrix NI-3/src/main.cpp
@@ -5,91 +5,65 @@
 uint32_t initStage = 0;
 uint64_t timestamp = 0;
 
-bool triggered = false;
-static uint64_t debounceTimeout = 300;
-uint64_t debounceTimer = 0;
-int32_t triggerCounter = 0;
+static int32_t debounceThresold = 2; // need that many consecutive readings for an edge
+int32_t debounceCounter = 0;
+int32_t inputState = HIGH;
 
-uint64_t lastIsr = 0;
+int64_t pulseCounter = 0;
+
 uint64_t task100msTimer = 0;
+uint64_t task1sTimer = 0;
 
-void onInterrupt()
-{
-  uint64_t timestamp = millis();
+int32_t inputPin = LOW;
 
-  if ((timestamp - lastIsr) > 150)
-  {
-    digitalWrite(LED_BUILTIN, 1);
-    triggered = true;
-    debounceTimer = timestamp;
-    lastIsr = timestamp;
-  }
-}
-
-
-size_t print64(uint64_t number, int base)
-{
-    size_t n = 0;
-    unsigned char buf[64];
-    uint8_t i = 0;
-
-    if (number == 0)
-    {
-        n += Serial.print((char)'0');
-        return n;
-    }
-
-    if (base < 2) base = 2;
-    else if (base > 16) base = 16;
-
-    while (number > 0)
-    {
-        uint64_t q = number/base;
-        buf[i++] = number - q*base;
-        number = q;
-    }
-
-    for (; i > 0; i--)
-    n += Serial.write((char) (buf[i - 1] < 10 ?
-    '0' + buf[i - 1] :
-    'A' + buf[i - 1] - 10));
-
-    return n;
-}
+size_t print64(uint64_t number, int base = 10);
 
 void setup()
 {
-  // put your setup code here, to run once:
-  timestamp = millis();
-  pinMode(LED_BUILTIN, OUTPUT); // Set Pin 13 as OUTPUT
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(2, INPUT_PULLUP);
 
   // Setup serial connection for debugging
   Serial.begin(115200U);
   delay(100);
-  Serial.println();
-  Serial.println("[  INIT  ] Begin");
-  initStage++;
-
-  attachInterrupt(digitalPinToInterrupt(REED_INPUT), onInterrupt, FALLING); //  function for creating external interrupts at pin2 on Rising (LOW to HIGH)
+  Serial.println("Init ok");
 }
 
 void loop()
 {
   timestamp = millis();
 
-  if (triggered && ((timestamp - debounceTimer) > debounceTimeout))
+  // 1s Task
+  if ((timestamp - task1sTimer) > 1000L)
   {
-    if (digitalRead(REED_INPUT))
-    {
-      triggerCounter++;
-      // Serial.print("[  ISR   ] Trigger! ");
-      Serial.print("{\"counter\":");Serial.print(triggerCounter);
-      Serial.print(",\"timestamp\":");print64(timestamp, 10);
-      Serial.println("}");
-    }
+    task1sTimer = timestamp;
+    // Serial.print((inputPin == HIGH) ? 'X' : '.');
 
-    triggered = false;
+    if ((inputState == LOW) && (inputPin == HIGH))
+    {
+      debounceCounter++;
+      if (debounceCounter >= debounceThresold)
+      {
+        inputState = HIGH;
+        debounceCounter = 0;
+      }
+    }
+    else if ((inputState == HIGH) && (inputPin == LOW))
+    {
+      debounceCounter++;
+      if (debounceCounter >= debounceThresold)
+      {
+        inputState = LOW;
+        debounceCounter = 0;
+        pulseCounter++;
+
+        Serial.print("{\"pulseCounter\":");
+        print64(pulseCounter);
+        Serial.print(",\"timestamp\":");
+        print64(timestamp);
+        Serial.println("}");
+      }
+    }
     return;
   }
 
@@ -97,7 +71,38 @@ void loop()
   if ((timestamp - task100msTimer) > 100L)
   {
     task100msTimer = timestamp;
-    digitalWrite(LED_BUILTIN, 0);
+    inputPin = digitalRead(REED_INPUT);
+    digitalWrite(LED_BUILTIN, HIGH - inputPin);
     return;
   }
+}
+
+size_t print64(uint64_t number, int base)
+{
+  size_t n = 0;
+  unsigned char buf[64];
+  uint8_t i = 0;
+
+  if (number == 0)
+  {
+    n += Serial.print((char)'0');
+    return n;
+  }
+
+  if (base < 2)
+    base = 2;
+  else if (base > 16)
+    base = 16;
+
+  while (number > 0)
+  {
+    uint64_t q = number / base;
+    buf[i++] = number - q * base;
+    number = q;
+  }
+
+  for (; i > 0; i--)
+    n += Serial.write((char)(buf[i - 1] < 10 ? '0' + buf[i - 1] : 'A' + buf[i - 1] - 10));
+
+  return n;
 }
